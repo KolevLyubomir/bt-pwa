ff732143bf2fb031f96dc1f01b5e8877fe8f92bb
KolevLyubomir <148394419+KolevLyubomir@users.noreply.github.com>
2025-09-22 03:06:55 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('bt-cache-v2').then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./sw.js','./chart.local.js','./assets/icons/icon-192.png','./assets/icons/icon-512.png'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
